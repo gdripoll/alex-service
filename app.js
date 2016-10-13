e29e10db9f4c0b5c1a8e72d41de3885afe281e60
goose <gdripoll@gmail.com>
2016-10-12 21:10:47 -0300

    estructura

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var Alex = require('./src/alex');
+
 //
 // config
 //
@@ -14,18 +16,16 @@ config.fs.root = path.join(__dirname,config.fs.root);
 //
 // routes
 //
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./src/index');
+var users = require('./src/users');
 
 
 var app = express();
-
 // view engine setup
 app.set('config',config);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.set('json spaces', 2);
-
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -37,10 +37,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-var alex = require('./routes/alex');
-app.use('/alex', alex);
-app.use('/', routes);
-app.use('/users', users);
+var alex = new Alex();
+app.use('/alex', alex.routes());
+
+// app.use('/', routes);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -53,15 +54,15 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
+// if (app.get('env') === 'development') {
+//     app.use(function(err, req, res, next) {
+//         res.status(err.status || 500);
+//         res.render('error', {
+//             message: err.message,
+//             error: err
+//         });
+//     });
+// }
 
 // production error handler
 // no stacktraces leaked to user
